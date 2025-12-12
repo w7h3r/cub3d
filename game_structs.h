1064ef6097e7b070806571392bc11dd512ee3391
Muhammet Arif Okcan <muokcan@k2m02s05.42kocaeli.com.tr>
2025-12-12 17:29:22 +0300

    Upload files

---
+++ game_structs.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   game_structs.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muokcan <muokcan@student.42kocaeli.com.tr  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/12 17:03:13 by muokcan           #+#    #+#             */
+/*   Updated: 2025/12/12 17:07:24 by muokcan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GAME_STRUCTS_H
+# define GAME_STRUCTS_H
+
+# include "lib/minilibx-linux/mlx.h"
+
+typedef struct	s_mlx
+{
+
+}	t_mlx;
+
+typedef struct	s_game
+{
+
+}	t_game;
+
+#endif
