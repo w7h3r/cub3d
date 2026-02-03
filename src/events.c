50b85a35aaeeefb3cb6f119464cffed00cf62b77
w7h3r <aarifokcan@gmail.com>
2026-02-03 07:23:49 +0300

    Delete redundant funcs

--- src/events.c
+++ src/events.c
@@ -6,7 +6,7 @@
 /*   By: muokcan <muokcan@student.42kocaeli.com.tr  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/25 19:49:30 by muokcan           #+#    #+#             */
-/*   Updated: 2026/01/27 02:49:44 by muokcan          ###   ########.fr       */
+/*   Updated: 2026/02/03 06:47:21 by muokcan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,9 @@
 
 void	hook_events(t_data *data)
 {
-	mlx_hook(data->mlx->win, 2, 1L << 0, key_press, data);
-	mlx_hook(data->mlx->win, 3, 1L << 1, key_release, data);
+	mlx_hook(data->mlx->win, 2, 1, key_press, data);
+	mlx_hook(data->mlx->win, 3, 2, key_release, data);
 	mlx_hook(data->mlx->win, 17, 0, exit_program, data);
-}
-
-int	is_player_moving(t_key_state *key_state)
-{
-	return (key_state->w_key || key_state->a_key || key_state->s_key
-		|| key_state->d_key || key_state->left_arrow || key_state->right_arrow);
 }
 
 void	detect_determ_input(t_player *player)
