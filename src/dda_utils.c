0f4922983504d45bd32a32e74f8b99c023d57bef
w7h3r <aarifokcan@gmail.com>
2026-02-06 00:20:14 +0300

    Swap function into main-dda file

--- src/dda_utils.c
+++ src/dda_utils.c
@@ -12,30 +12,6 @@
 
 #include "../inc/cub3d.h"
 
-void	dda_iter(t_data *data, t_ray *ray)
-{
-	if (ray->dda.side_dist_x < ray->dda.side_dist_y)
-	{
-		ray->dda.side_dist_x += ray->dda.delta_dist_x;
-		ray->map_x += ray->dda.step_x;
-		ray->dda.side = 0;
-	}
-	else
-	{
-		ray->dda.side_dist_y += ray->dda.delta_dist_y;
-		ray->map_y += ray->dda.step_y;
-		ray->dda.side = 1;
-	}
-	if (ray->map_y >= 0 && ray->map_y < data->map->height
-		&& ray->map_x >= 0 && ray->map_x < data->map->width)
-	{
-		if (data->map->map_grids[ray->map_y][ray->map_x] == '1')
-			ray->dda.did_hit_wall = 1;
-	}
-	else
-		ray->dda.did_hit_wall = 1;
-}
-
 void	init_dda_vars_x(t_ray *ray, t_player *player)
 {
 	if (ray->ray_dir_x < 0)
